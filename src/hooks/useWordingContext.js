6542b4aceaecd14ca362247d214a2aecf95340e6
mplacke <agos@gmx.net>
2023-06-21 13:42:35 +0200

    Prevent non loaded wording context to render children

--- src/hooks/useWordingContext.js
+++ src/hooks/useWordingContext.js
@@ -14,7 +14,10 @@ var WordingContextProvider = function (_a) {
     var _b = (0, react_1.useState)(DEFAULT), value = _b[0], setValue = _b[1];
     (0, react_1.useEffect)(function () {
         var resultHandler = function (result) { return setValue(result.default); };
-        var errorHandler = function (reason) { return console.log("Failed to load wordings for locale ".concat(locale, ". Reason:"), reason); };
+        var errorHandler = function (reason) {
+            console.log("Failed to load wordings for locale ".concat(locale, ". Reason:"), reason);
+            setValue(DEFAULT);
+        };
         wordingResolver(locale)
             .then(resultHandler)
             .catch(errorHandler);
@@ -24,6 +27,6 @@ var WordingContextProvider = function (_a) {
         };
     }, [locale, wordingResolver]);
     var Provider = CONTEXT.Provider;
-    return (0, jsx_runtime_1.jsx)(Provider, { value: value, children: children });
+    return (0, jsx_runtime_1.jsx)(Provider, { value: value, children: value !== DEFAULT && children });
 };
 exports.WordingContextProvider = WordingContextProvider;
--- src/hooks/useWordingContext.tsx
+++ src/hooks/useWordingContext.tsx
@@ -1,5 +1,5 @@
-import { FC, PropsWithChildren, createContext, useContext, useEffect, useState } from 'react';
-import { useLocale } from './useLocale';
+import {FC, PropsWithChildren, createContext, useContext, useEffect, useState} from 'react';
+import {useLocale} from './useLocale';
 
 const DEFAULT = {};
 
@@ -13,22 +13,25 @@ type Props = PropsWithChildren & {
     wordingResolver: WordingResolver;
 };
 
-export const WordingContextProvider: FC<Props> = ({ children, wordingResolver }) => {
+export const WordingContextProvider: FC<Props> = ({children, wordingResolver}) => {
     const locale = useLocale();
     const [value, setValue] = useState<any>(DEFAULT);
     useEffect(() => {
         let resultHandler = (result: any) => setValue(result.default);
-        let errorHandler = (reason: any) => console.log(`Failed to load wordings for locale ${locale}. Reason:`, reason);
+        let errorHandler = (reason: any) => {
+            console.log(`Failed to load wordings for locale ${locale}. Reason:`, reason);
+            setValue(DEFAULT);
+        };
         wordingResolver(locale)
             .then(resultHandler)
             .catch(errorHandler);
         return () => {
             resultHandler = () => { };
             errorHandler = () => { };
-        }
+        };
     }, [locale, wordingResolver]);
     const Provider = CONTEXT.Provider;
     return <Provider value={value}>
-        {children}
-    </Provider>
-}
+        {value !== DEFAULT && children}
+    </Provider>;
+};
