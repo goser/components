fdbed68dc7eea84ad92b4b7fe982b26f7255ecee
Michael Placke <mplacke@magix.net>
2024-06-25 14:51:38 +0200

    Add autoFocus to Button component

--- dist/Button/Button.d.ts
+++ dist/Button/Button.d.ts
@@ -1,6 +1,7 @@
 import { FC, MouseEventHandler, PropsWithChildren } from 'react';
 import { PropsWithStyle } from '../types/PropsWithStyle';
 type Props = {
+    autoFocus?: boolean;
     onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
     disabled?: boolean;
     title?: string;
